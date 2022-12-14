0ca4451002c93312b7f6f9e87aac304bcf0949a4
EvaShamordina <evashamordina09@gmail.com>
2022-12-14 21:09:48 +0300

    Initial commit

---
+++ HomeWork9WindowsFormsApp1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HomeWork9WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Visible = true;
+            button1.Visible = false;
+            Form1().ActiveForm.BackColor = Color.Black;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button2.Visible = false;
+            button1.Visible = true;
+            Form1().ActiveForm.BackColor = Color.White;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
